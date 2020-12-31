bb0ba03ef87583ad96b45fbcf44d573197cb5dc2
yusuf963 <mohammad963yusuf@gmail.com>
2020-12-31 16:06:17 +0000

    adding dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import StylePhoto from "./img/design2.png";
 
-function App() {
+const Header = (props) => {
+  return <h1>Welcoem to {props.name} Resturnat</h1>;
+};
+
+const dishes = ["Fish and chips", "french fries", "salad", "toast"];
+const dishObject = dishes.map((dish, i) => ({ id: i, title: dish }));
+
+const Main = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* ul data from array */}
+      <ul>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+      {/* ends */}
+      <ul>
+        <li>Beans</li>
+        <li>Salad</li>
+        <li>French fries</li>
+        <li>Pepsi</li>
+      </ul>
     </div>
   );
-}
+};
+
+const Footer = (props) => {
+  return <p>Please visit next {props.year} for discount</p>;
+};
+
+const Menu = () => {
+  return (
+    <ul>
+      <li>Contact</li>
+      <li>Project</li>
+      <li> About</li>
+      <li> Reach out</li>
+    </ul>
+  );
+};
+
+const Photo = () => {
+  return <img src={StylePhoto} alt={"hh"} />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header name={"Ahmad"} />
+      <Main dishes={dishObject} />
+      <Footer year={new Date().getFullYear()} />
+      <Menu />
+      <Photo />
+    </div>
+  );
+};
 
 export default App;
