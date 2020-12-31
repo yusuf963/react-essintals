4088a245fbb7b178d2829a5b68f82a00e517e46c
yusuf963 <mohammad963yusuf@gmail.com>
2020-12-31 16:05:31 +0000

    adding test compoenent

---
+++ src/Test.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const SecretComponent = () => {
+  return <h1>Here is a secret component appears</h1>;
+};
+const RegularComponent = () => {
+  return <h1>Here is a Regular component appears</h1>;
+};
+
+// destructuring arrays
+const array = ["apple", "banana", "wheat"];
+const [, second] = array;
+console.log(second);
+
+const Test = () => {
+  let [emotion, setEmotion] = useState("");
+  let [count, setCount] = useState(0);
+  if (count < 9) {
+    emotion = "";
+  } else if (count === 9) {
+    emotion = "Hangury";
+  } else {
+    emotion = "Happy";
+  }
+
+  return (
+    <>
+      <h1>Testing {emotion} UseState functionality</h1>
+      <button onClick={() => setEmotion("Sad")}>Click me if you are Sad</button>
+      <button onClick={() => setEmotion("Happy")}>
+        Click me if uyou are Happy
+      </button>
+      <button onClick={() => setEmotion("Hangury")}>
+        Click me if you are Hungry
+      </button>
+      <h1>Testing count State</h1>
+      <h1>{emotion}</h1>
+
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>Increas Counting</button>
+      <button onClick={() => setCount(0)}> Reset Count to zero</button>
+      <button onClick={() => setCount(count - 1)}>Decreas Counting</button>
+    </>
+  );
+};
+
+export default Test;
